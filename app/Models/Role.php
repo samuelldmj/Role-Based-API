8fa79e2e1fa0e3b04439a311001645d037e1d651
Samuel <samuelldmj5@gmail.com>
2025-12-14 16:10:30 +0100

    some changes in the permission, roles migration files

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+    ];
+
+
+    public function permission
 }
