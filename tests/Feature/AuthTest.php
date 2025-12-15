bb10d76edf9d28c5c54be14625d960d51d6ee7a4
Samuel <samuelldmj5@gmail.com>
2025-12-15 20:04:29 +0100

    test file

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
